6d1051719b5275d50041356e5640d85f2d4db355
jirum <monteclar.jerome19@gmail.com>
2022-07-08 09:41:22 +0800

    create validation for long url

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,6 +1,7 @@
 class Url < ApplicationRecord
   validates_presence_of :description
   validates_presence_of :long_url
-
+  validates_format_of :long_url, :with => /\A(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\.-]*)*\/?\Z/i
   belongs_to :user
+
 end
