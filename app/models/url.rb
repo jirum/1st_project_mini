2e99f1668c0151ff7e05ba1be5ef2612ffa38b6c
jirum <monteclar.jerome19@gmail.com>
2022-07-08 12:54:04 +0800

    adding function generate short url in model

--- app/models/url.rb
+++ app/models/url.rb
@@ -4,4 +4,13 @@ class Url < ApplicationRecord
   validates_format_of :long_url, :with => /\A(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\.-]*)*\/?\Z/i
   belongs_to :user
 
+  before_create :generate_short_url
+
+  def generate_short_url
+    loop do
+      @short = SecureRandom.base64(6)
+      break if not Url.exists?(short_url: @short)
+    end
+    self.short_url=@short
+  end
 end
