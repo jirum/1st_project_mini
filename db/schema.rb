1a7eeefc365f08477be15644ad0c68d3c564eec0
jirum <monteclar.jerome19@gmail.com>
2022-07-13 15:02:07 +0800

    add column to table urls

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_06_085529) do
+ActiveRecord::Schema.define(version: 2022_07_13_054932) do
 
   create_table "urls", charset: "utf8mb4", force: :cascade do |t|
     t.string "description"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2022_07_06_085529) do
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
     t.index ["user_id"], name: "index_urls_on_user_id"
   end
 
