c561793206a817dd27aa1b10f5cdba1a4d4373f9
jirum <monteclar.jerome19@gmail.com>
2022-07-15 13:19:32 +0800

    create model for region

---
+++ app/models/region.rb
@@ -0,0 +1,6 @@
+class Region < ApplicationRecord
+  validates_presence_of :code
+  validates_presence_of :name
+  validates_presence_of :region_name
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_13_054932) do
+ActiveRecord::Schema.define(version: 2022_07_15_045517) do
+
+  create_table "regions", charset: "utf8mb4", force: :cascade do |t|
+    t.string "code"
+    t.string "name"
+    t.string "region_name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "urls", charset: "utf8mb4", force: :cascade do |t|
     t.string "description"
