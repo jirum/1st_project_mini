53e0918717401d11011efb27bb3b13aa50143a6c
jirum <monteclar.jerome19@gmail.com>
2022-07-15 17:58:57 +0800

    add region in serializer and controller api

---
+++ app/controllers/api/regions_controller.rb
@@ -0,0 +1,10 @@
+class Api::RegionsController < ApplicationController
+  def index
+    @regions = Region.all
+    respond_to do |format|
+      format.html
+      format.json  {render json: @regions, each_serializer: RegionSerializer }
+    end
+  end
+end
+
